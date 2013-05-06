e1a778f234d5b0c61d03d21bf88c0b1a561f4c8a
Sebastian Strauß <sstrauss@bizolution.de>
2013-05-06 11:56:07 +0200

    Magic numbers entfernt (fix #1)

--- Program.cs
+++ Program.cs
@@ -9,29 +9,27 @@ namespace Enten
     {
         static void Main(string[] args)
         {
-            Ente[] oEnten = new Ente[3];
+            List<Ente> oEntenschwarm = new List<Ente>();
             Random oRND = new Random();
-            oEnten[0] = new Krickente();
-            oEnten[1] = new Stockente();
-            oEnten[2] = new Schnatterente();
+            oEntenschwarm.Add(new Krickente());
+            oEntenschwarm.Add(new Stockente());
+            oEntenschwarm.Add(new Schnatterente());
 
-            Console.WriteLine(String.Format("Es wurden Folgende Enten geladen:" +
-                "\n {0} \n {1} \n {2}"
-                , oEnten[0].GetType().ToString()
-                , oEnten[1].GetType().ToString()
-                , oEnten[2].GetType().ToString()));
+            Console.WriteLine("Es wurden Folgende Enten geladen:");
+            foreach (Ente oEnte in oEntenschwarm)
+               Console.WriteLine(oEnte.GetType());        
 
             do
             {
-                int iEntennummer = oRND.Next(0, 3);
-                string Entenname = oEnten[iEntennummer].GetType().ToString();
-                Entenname = Entenname.Substring(Entenname.IndexOf('.')+ 1);
+                int iEntennummer = oRND.Next(0, oEntenschwarm.Count());
+                string Entenname = oEntenschwarm[iEntennummer].GetType().ToString();
+                Entenname = Entenname.Substring(Entenname.IndexOf('.') + 1);
                 Console.Write(Entenname);
                 Console.Write(": ");
                 if(oRND.Next(0,2) == 0)
-                    Console.WriteLine(oEnten[iEntennummer].Quacken());
+                    Console.WriteLine(oEntenschwarm[iEntennummer].Quacken());
                 else
-                    Console.WriteLine(oEnten[iEntennummer].Schwimmen());
+                    Console.WriteLine(oEntenschwarm[iEntennummer].Schwimmen());
                 System.Threading.Thread.Sleep(1000);   
             } while (true);
         }
